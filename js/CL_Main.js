c62c424740682dd1b11bd940c8d9889fca99b1db
Roximar Segura <1001.30517704.ucla@gmail.com>
2025-03-28 21:14:46 -0400

    ok

--- js/CL_Main.js
+++ js/CL_Main.js
@@ -4,9 +4,9 @@ Sea la clase Cl_empleado con atributo sueldoBase y método calcularSueldo(), que
 retorna sueldoBase. La clase Cl_obrero hereda de Cl_empleado y también tiene horas extras a
 $2 cada una; la clase Cl_administrativo hereda de Cl_empleado y tiene una prima fija. El
 método calcularSueldo() debe hacer lo indicado en cada clase hija.
-Ej.
+Ej:
 - calcularSueldo() en obrero es: this.sueldoBase + this.horasExtras * 2
-- calcularSueldo() en administrativo es: this.sueldoBase + this.primaFija
+- calcularSueldo() en administrativo es: this.sueldoBase + this.primaFija.
  */
 
 import Cl_Obrero from "./CL_Obrero.js";
